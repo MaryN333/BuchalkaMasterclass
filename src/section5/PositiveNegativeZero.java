bf47d2bce678fdde4ae3b697e8d3c744254e8c3f
MaryN333 <sidyakina@gmail.com>
2024-09-05 17:32:17 +0200

    Solution to the exercise PositiveNegativeZero. Сloses #1

--- src/section5/PositiveNegativeZero.java
+++ src/section5/PositiveNegativeZero.java
@@ -7,7 +7,6 @@ public class PositiveNegativeZero {
         checkNumber(0);
     }
 
-
     // write code here
     public static void checkNumber (int number) {
         System.out.println((number > 0) ? "positive":((number < 0)? "negative": "zero"));
